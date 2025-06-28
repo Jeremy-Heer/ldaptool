363617622a4c2ceba6792cd8950c8617f71e0500
Jeremy Heer <jqheer@gmail.com>
2025-06-28 11:13:44 -0400

    initial commit

---
+++ src/main/java/com/heer/ldaptool/LdaptoolApplication.java
@@ -0,0 +1,13 @@
+package com.heer.ldaptool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdaptoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdaptoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heer/ldaptool/LdaptoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heer.ldaptool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdaptoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
